6caddd96183cf1549bf604be9c21fc0e29c20e53
AruEld <a.seraliyeva@kbtu.kz>
2025-06-02 07:35:02 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 import io
 
 # Load model
-cnn_model = tf.keras.models.load_model("cnn_motor_fault.h5")
+cnn_model = tf.keras.models.load_model("cnn_motor_fault.keras")
 
 # File uploader
 uploaded_file = st.file_uploader("Upload WAV file", type=["wav"])
