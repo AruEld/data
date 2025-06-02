44241663e653c5262c3185c5d976ce8263617a74
AruEld <a.seraliyeva@kbtu.kz>
2025-06-02 05:44:22 +0500

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import numpy as np
+import librosa
+import librosa.display
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import io
+
+# Load model
+cnn_model = tf.keras.models.load_model("cnn_motor_fault.h5")
+
+# File uploader
+uploaded_file = st.file_uploader("Upload WAV file", type=["wav"])
+if uploaded_file:
+    y, sr = librosa.load(uploaded_file, sr=16000)
+    
+    # Generate and display spectrogram
+    st.audio(uploaded_file)
+    st.write("### Mel Spectrogram")
+    S = librosa.feature.melspectrogram(y, sr=sr, n_mels=128)
+    S_dB = librosa.power_to_db(S, ref=np.max)
+    
+    fig, ax = plt.subplots()
+    img = librosa.display.specshow(S_dB, sr=sr, ax=ax)
+    st.pyplot(fig)
+
+    # Prepare input
+    S_resized = tf.image.resize(S_dB[..., np.newaxis], (128, 256)).numpy()
+    S_input = np.expand_dims(S_resized, axis=0)
+
+    # Predict
+    preds = cnn_model.predict(S_input)
+    pred_class = np.argmax(preds)
+
+    st.write(f"### Predicted Class: {pred_class}")
